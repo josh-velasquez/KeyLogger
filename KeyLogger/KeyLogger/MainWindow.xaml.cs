5505994eb7b953cc83a2c5cd4e938226d6b58474
Josh <joshvelasqez6@gmail.com>
2020-01-28 15:11:30 -0700

    Initial commit. Transported key logger with sample release build

---
+++ KeyLogger/KeyLogger/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using KeyLogger.KeyboardUtil;
+using System.Windows;
+
+namespace KeyLogger
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        KeyboardListener listener;
+        private void OnStartRecorderClick(object sender, RoutedEventArgs e)
+        {
+            listener = new KeyboardListener();
+            listener.KeyDown += new RawKeyEventHandler(KeyboardRecorder);
+            clear.IsEnabled = false;
+            start.IsEnabled = false;
+        }
+
+        private void KeyboardRecorder(object sender, RawKeyEventArgs args)
+        {
+            keyboard_textbox.Text += args.ToString();
+        }
+
+        private void OnStopRecorderClick(object sender, RoutedEventArgs e)
+        {
+            start.IsEnabled = true;
+            clear.IsEnabled = true;
+            listener.Dispose();
+        }
+
+        private void OnClearRecorderClick(object sender, RoutedEventArgs e)
+        {
+            keyboard_textbox.Clear();
+        }
+
+    }
+}
